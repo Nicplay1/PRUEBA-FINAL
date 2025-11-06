3b2c11d453d38e9c6e0e17d8eca487bcc8031915
Nicplay1 <nicolasballesteros900@gmail.com>
2025-11-05 19:46:51 -0500

    correcion

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,11 +73,8 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 # ---------------------------------------
